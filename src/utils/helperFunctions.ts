43e76cb6f44ea591f4a7764d6350d26b3e409b3c
Fabian Lopez Gonzalez <fabianlopez@pm.me>
2023-07-05 11:47:41 -0400

    Made function to format dates. Created links to categories

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -1,8 +1,10 @@
-import { strictEqual } from "assert";
+import { strictEqual } from 'assert';
+import { getFromAPI } from './api';
+import { CategoryFields, Endpoints } from 'src/types';
 
 let ellipsis = /\[.*?\]/g; // match anything in square brackets
 let emptySpace = /&.*?;/g; // match anything between & and ;
-let openDiv = /<div>/g; 
+let openDiv = /<div>/g;
 let closingDiv = /<\/div>/g;
 
 export function removeHTMLEntity(str: string) {
@@ -12,16 +14,29 @@ export function removeHTMLEntity(str: string) {
     newStr = str.replace(ellipsis, '...');
   }
   if (emptySpace.test(str)) str.replace(emptySpace, ' ');
-  return newStr
+  return newStr;
 }
 
 export function divForP(str: string) {
   let newStr;
-  if (openDiv.test(str)) {newStr = str.replace(openDiv, '<p>').replace(closingDiv, '</p>');}
-  return newStr;
+  if (openDiv.test(str)) {
+    newStr = str.replace(openDiv, '<p>').replace(closingDiv, '</p>');
+    return newStr;
+  } else {
+    return str;
+  }
 }
 
+export function dateConverter(date: string) {
+  const newDate = new Date(date);
+  return `${new Intl.DateTimeFormat('ca', { month: 'long' }).format(
+    newDate
+  )} ${new Intl.DateTimeFormat('ca', { day: 'numeric' }).format(
+    newDate
+  )}, ${new Intl.DateTimeFormat('ca', { year: 'numeric' }).format(newDate)}`;
+}
 
-
-
-
+export function categoryMapper(allCategories, postCategories: number[]) {
+  const categories = allCategories.filter(category => postCategories.indexOf(category.id) !== -1);
+  return categories;
+}
