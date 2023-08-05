ddd6b08a7b4f0cbd42041ff7f234c0ca99920f1e
fabianlopezdev <fabianlopez@pm.me>
2023-08-05 18:35:12 -0400

    WIP top-footer-section

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -2,3 +2,26 @@ export const bottomFooterLinks = [
   { name: `Avís Legal`, slug: 'avis-legal' },
   { name: `Política de cookies`, slug: 'politica-de-cookies' },
 ];
+
+export const topFooterLinks = [
+  {
+    name: `El club`,
+    links: [
+      `Estatuts`,
+      `Codi ètic`,
+      `Protocol prevenció violències sexuals`,
+      `Assegurança esportiva obligtòria`,
+    ],
+  },
+  {
+    name: `Equips`,
+    links: [
+      `Escola de bàsquet`,
+      `Bàsquet en cadira de rodes`,
+      `Masculins`,
+      `Femenins`,
+    ],
+  },
+  { name: `Altres`, links: [`Escola d'iniciació`, `Campus estiu 2023`, `Tecnificació skills camp`, `FAQS: 10 preguntes i 10 respostes`] },
+
+];
