f493e8e833efd59360980a65c587b1c586954235
fabianlopezdev <fabianlopez@pm.me>
2023-09-18 14:10:57 -0400

    Testing Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import nodejs from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
   adapter: nodejs({
-    mode: "standalone"
-  })
+    mode: "middleware"
+  }),
+  output: "hybrid",
 });
