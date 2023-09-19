438f98d78382a7b6cb60603b25fb8b1edf9e940e
fabianlopezdev <fabianlopez@pm.me>
2023-09-19 13:56:31 -0400

    Hero Section: sponsors container layout made

---
+++ src/data/sponsors.ts
@@ -0,0 +1,26 @@
+export const mainSponsorsWhite = [
+  {
+    name: `Pisos punto com`,
+    logo: `/sponsors/pisos-punto-com-logo-blanc.png`,
+    alt: `Logo de pisos.com`,
+    link: `https://www.pisos.com/`,
+  },
+  {
+    name: `Ajuntament de Granollers`,
+    logo: `/sponsors/ajuntament-de-granollers-logo-blanc.png`,
+    alt: `Logo de l'ajuntament de Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Granollers`,
+    logo: `/sponsors/granollers-logo-blanc.png`,
+    alt: `Logo Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Diputació de Barcelona`,
+    logo: `/sponsors/diputació-de-barcelona-logo-blanc.png`,
+    alt: `Logo de la diputació de Barcelona`,
+    link: `https://www.diba.cat/`,
+  },
+];
