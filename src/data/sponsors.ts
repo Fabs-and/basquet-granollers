4614e23c1386e7cb7fecbd9f61fce24f7c6c70fc
fabianlopezdev <fabianlopez@pm.me>
2023-09-20 17:48:48 -0400

    Sponsors: Made diferent component for the sponsor containers. Working in the sponsors section above the footer

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -1,25 +1,52 @@
 export const mainSponsorsWhite = [
   {
     name: `Pisos punto com`,
-    logo: `/sponsors/pisos-punto-com-logo-blanc.png`,
+    logo: `/sponsors/main/pisos-punto-com-logo-blanc.png`,
     alt: `Logo de pisos.com`,
     link: `https://www.pisos.com/`,
   },
   {
     name: `Ajuntament de Granollers`,
-    logo: `/sponsors/ajuntament-de-granollers-logo-blanc.png`,
+    logo: `/sponsors/main/ajuntament-de-granollers-logo-blanc.png`,
     alt: `Logo de l'ajuntament de Granollers`,
     link: `https://www.granollers.cat/`,
   },
   {
     name: `Granollers`,
-    logo: `/sponsors/granollers-logo-blanc.png`,
+    logo: `/sponsors/main/granollers-logo-blanc.png`,
     alt: `Logo Granollers`,
     link: `https://www.granollers.cat/`,
   },
   {
     name: `Diputació de Barcelona`,
-    logo: `/sponsors/diputació-de-barcelona-logo-blanc.png`,
+    logo: `/sponsors/main/diputació-de-barcelona-logo-blanc.png`,
+    alt: `Logo de la diputació de Barcelona`,
+    link: `https://www.diba.cat/`,
+  },
+];
+
+export const mainSponsorsBlue = [
+  {
+    name: `Pisos punto com`,
+    logo: `/sponsors/main/pisos-punto-com-logo-blau.png`,
+    alt: `Logo de pisos.com`,
+    link: `https://www.pisos.com/`,
+  },
+  {
+    name: `Ajuntament de Granollers`,
+    logo: `/sponsors/main/ajuntament-de-granollers-logo-blau.png`,
+    alt: `Logo de l'ajuntament de Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Granollers`,
+    logo: `/sponsors/main/granollers-logo-blau.png`,
+    alt: `Logo Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Diputació de Barcelona`,
+    logo: `/sponsors/main/diputació-de-barcelona-logo-blau.png`,
     alt: `Logo de la diputació de Barcelona`,
     link: `https://www.diba.cat/`,
   },
