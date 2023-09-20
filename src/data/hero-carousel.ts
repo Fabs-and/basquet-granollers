e0ebd15fbbba36b42667eb238a39a837f5efb36d
fabianlopezdev <fabianlopez@pm.me>
2023-09-20 13:00:48 -0400

    Hero-section: layour finished

---
+++ src/data/hero-carousel.ts
@@ -0,0 +1,9 @@
+export const slides = [
+  {
+    image: `hero-image.png`,
+    title: `Coneix la nostra escola de bàsquet`,
+    description: `Treballem dia a dia per que aquest esport arribi al màxim de nens de la comarca i continuï sent el referent de moltes famílies a l’hora d’escollir un esport que aporti salut, valors socials i hàbits als seus fills.`,
+    link: `https://cbgranollers.cat/escola-de-basquet-temporada-22-23/`,
+    buttonText: `Veure més`
+  },
+];
