8a8e63938008a5babe3722afa25d320bfd83bc9d
fabianlopezdev <fabianlopez@pm.me>
2023-09-21 11:47:57 -0400

    Sponsors: first container after hero-section made responsive

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -51,3 +51,127 @@ export const mainSponsorsBlue = [
     link: `https://www.diba.cat/`,
   },
 ];
+
+//Other sponsors are by default blue
+export const otherSponsors = [
+  {
+    name: `Pisos punto com`,
+    logo: `/sponsors/main/pisos-punto-com-logo-blau.png`,
+    alt: `Logo de pisos.com`,
+    link: `https://www.pisos.com/`,
+  },
+  {
+    name: `Ajuntament de Granollers`,
+    logo: `/sponsors/main/ajuntament-de-granollers-logo-blau.png`,
+    alt: `Logo de l'ajuntament de Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Granollers`,
+    logo: `/sponsors/main/granollers-logo-blau.png`,
+    alt: `Logo Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Diputació de Barcelona`,
+    logo: `/sponsors/main/diputació-de-barcelona-logo-blau.png`,
+    alt: `Logo de la diputació de Barcelona`,
+    link: `https://www.diba.cat/`,
+  },
+  {
+    name: `Pisos punto com`,
+    logo: `/sponsors/main/pisos-punto-com-logo-blau.png`,
+    alt: `Logo de pisos.com`,
+    link: `https://www.pisos.com/`,
+  },
+  {
+    name: `Ajuntament de Granollers`,
+    logo: `/sponsors/main/ajuntament-de-granollers-logo-blau.png`,
+    alt: `Logo de l'ajuntament de Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Granollers`,
+    logo: `/sponsors/main/granollers-logo-blau.png`,
+    alt: `Logo Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Diputació de Barcelona`,
+    logo: `/sponsors/main/diputació-de-barcelona-logo-blau.png`,
+    alt: `Logo de la diputació de Barcelona`,
+    link: `https://www.diba.cat/`,
+  },
+  {
+    name: `Pisos punto com`,
+    logo: `/sponsors/main/pisos-punto-com-logo-blau.png`,
+    alt: `Logo de pisos.com`,
+    link: `https://www.pisos.com/`,
+  },
+  {
+    name: `Ajuntament de Granollers`,
+    logo: `/sponsors/main/ajuntament-de-granollers-logo-blau.png`,
+    alt: `Logo de l'ajuntament de Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Granollers`,
+    logo: `/sponsors/main/granollers-logo-blau.png`,
+    alt: `Logo Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Diputació de Barcelona`,
+    logo: `/sponsors/main/diputació-de-barcelona-logo-blau.png`,
+    alt: `Logo de la diputació de Barcelona`,
+    link: `https://www.diba.cat/`,
+  },
+  {
+    name: `Pisos punto com`,
+    logo: `/sponsors/main/pisos-punto-com-logo-blau.png`,
+    alt: `Logo de pisos.com`,
+    link: `https://www.pisos.com/`,
+  },
+  {
+    name: `Ajuntament de Granollers`,
+    logo: `/sponsors/main/ajuntament-de-granollers-logo-blau.png`,
+    alt: `Logo de l'ajuntament de Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Granollers`,
+    logo: `/sponsors/main/granollers-logo-blau.png`,
+    alt: `Logo Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Diputació de Barcelona`,
+    logo: `/sponsors/main/diputació-de-barcelona-logo-blau.png`,
+    alt: `Logo de la diputació de Barcelona`,
+    link: `https://www.diba.cat/`,
+  },
+  {
+    name: `Pisos punto com`,
+    logo: `/sponsors/main/pisos-punto-com-logo-blau.png`,
+    alt: `Logo de pisos.com`,
+    link: `https://www.pisos.com/`,
+  },
+  {
+    name: `Ajuntament de Granollers`,
+    logo: `/sponsors/main/ajuntament-de-granollers-logo-blau.png`,
+    alt: `Logo de l'ajuntament de Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Granollers`,
+    logo: `/sponsors/main/granollers-logo-blau.png`,
+    alt: `Logo Granollers`,
+    link: `https://www.granollers.cat/`,
+  },
+  {
+    name: `Diputació de Barcelona`,
+    logo: `/sponsors/main/diputació-de-barcelona-logo-blau.png`,
+    alt: `Logo de la diputació de Barcelona`,
+    link: `https://www.diba.cat/`,
+  },
+];
