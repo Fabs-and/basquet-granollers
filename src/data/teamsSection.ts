4d4e2ff248a05dc28141559ee046465dbcf2a0bd
fabianlopezdev <fabianlopez@pm.me>
2023-09-25 19:37:32 -0400

    Teams Section: Desktop: created teams mock data so the component can iterate through it

---
+++ src/data/teamsSection.ts
@@ -0,0 +1,26 @@
+export const teams = [
+  {
+    name: `Femenins`,
+    slug: `/`,
+    image: `/teams/femenins.png`,
+    alt: `Femenins`,
+  },
+  {
+    name: `Masculins`,
+    slug: `/`,
+    image: `/teams/masculins.png`,
+    alt: `Masculins`,
+  },
+  {
+    name: `Bàsquet en cadira de rodes`,
+    slug: `/`,
+    image: `/teams/cadira-de-rodes.png`,
+    alt: `Bàsquet en cadira de rodes`,
+  },
+  {
+    name: `Escola`,
+    slug: `/`,
+    image: `/teams/escola-de-basquet.png`,
+    alt: `Escola de bàsquet`,
+  },	
+]
