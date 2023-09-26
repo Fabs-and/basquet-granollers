0103b699b67b24f710d604acf27dd01969e2fb3d
fabianlopezdev <fabianlopez@pm.me>
2023-09-26 14:30:56 -0400

    Memberships-section: desktop membership article layour done

---
+++ src/data/memberships.ts
@@ -0,0 +1,23 @@
+export const memberships = [
+  {
+    name: `Soci/Sòcia d'honor`,
+    price: `150€`,
+    advantages: [
+      `Carnet exclusiu amb els 100 primers números`,
+      `Obsequi personalitzat CBG`,
+      `Seient a la llotja en els partits sènior masculí i sènior femení`,
+      `Invitació personalitzada a la presentació
+anual d’equips`,
+      `3 dies d’accés a la pista,
+segons disponibilitat`,
+      `Informació periòdica de les activitats
+del club i dels avenços dels equips`,
+      `Activitats familiars per als familiars`,
+      `Invitació a l’acte de tancament
+de temporada (botifarrada CBG)`,
+      `Invitació a l’acte de tancament
+de temporada (botifarrada CBG)`,
+      `Invitació a la trobada anual amics CBG`,
+    ],
+  },
+];
