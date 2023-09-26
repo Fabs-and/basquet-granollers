f0628bcd6273fd8e2ebb6b45e829ec1b09443caf
fabianlopezdev <fabianlopez@pm.me>
2023-09-26 14:38:21 -0400

    Memberships-section: created mocked data for each membership

--- src/data/memberships.ts
+++ src/data/memberships.ts
@@ -19,5 +19,39 @@ de temporada (botifarrada CBG)`,
 de temporada (botifarrada CBG)`,
       `Invitació a la trobada anual amics CBG`,
     ],
+    image: ``,
+    imageAlt: ``,
+  },
+  {
+    name: `Soci/Sòcia`,
+    price: `85€`,
+    advantages: [
+      `Invitació personalitzada a la presentació
+anual d’equips`,
+      `3 dies d’accés a la pista,
+segons disponibilitat`,
+      `Informació periòdica de les activitats
+del club i dels avenços dels equips`,
+      `Activitats familiars per als familiars`,
+      `Invitació a l’acte de tancament
+de temporada (botifarrada CBG)`,
+      `Invitació a la trobada anual amics CBG`,
+    ],
+    image: ``,
+    imageAlt: ``,
+  },
+  {
+    name: `Supporter`,
+    price: `30€`,
+    advantages: [
+      `Informació periòdica de les activitats
+del club i dels avenços dels equips`,
+      `Activitats familiars per als familiars`,
+      `Invitació a l’acte de tancament
+de temporada (botifarrada CBG)`,
+      `Invitació a la trobada anual amics CBG`,
+    ],
+    image: ``,
+    imageAlt: ``,
   },
 ];
