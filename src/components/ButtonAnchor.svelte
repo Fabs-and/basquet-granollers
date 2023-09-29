010ceacb1c34125fb44cf2207cc1144a2ddd9a43
fabianlopezdev <fabianlopez@pm.me>
2023-09-29 14:07:19 -0400

    ButtonAnchors: made global classes for both Astro and Svelte ButtonAnchors components

--- src/components/ButtonAnchor.svelte
+++ src/components/ButtonAnchor.svelte
@@ -13,29 +13,12 @@
 </script>
 
 
-<a href={slug} style='--hoverTextColor:{hoverTextColor}; --textColor: {textColor}' class:isFilled class:hidden class:isSmallBtn>{text.toUpperCase()}</a>
+<a class='button-anchor' href={slug} style='--hoverTextColor:{hoverTextColor}; --textColor: {textColor}' class:isFilled class:hidden class:isSmallBtn>{text.toUpperCase()}</a>
 
 
 <style >
   .hidden {
     display: none;
-  }
-  
-  a {
-    text-decoration: none;
-    display: grid;
-    place-items: center;
-    height: var(--btn-height);
-    width: var(--btn-width-regular);
-    border: 1px solid var(--clr-accent);
-    border-radius: 1.75rem;
-    color: var(--textColor);
-  }
-  
-  a:hover {
-    cursor: pointer;
-    background-color: var(--clr-accent);
-    color: var(--hoverTextColor)
   }
 
   .isFilled {
