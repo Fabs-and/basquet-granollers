0189b8d37e65a51a868d7f336b30d8e642ffb848
fabianlopezdev <fabianlopez@pm.me>
2023-10-12 12:26:40 -0400

    HomePage buttons modified their layout to better represent the figma design

--- src/components/ButtonAnchor.svelte
+++ src/components/ButtonAnchor.svelte
@@ -7,12 +7,12 @@
   export let textColor;
   export let hoverTextColor;
   export let hidden = false;
-  export let isSmallBtn =false;
+  
 
 </script>
 
 
-<a class='button-anchor' href={slug} style='--hoverTextColor:{hoverTextColor}; --textColor: {textColor}' class:isFilled class:hidden class:isSmallBtn>{text.toUpperCase()}</a>
+<a class='button-anchor' href={slug} style='--hoverTextColor:{hoverTextColor}; --textColor: {textColor}' class:isFilled class:hidden >{text.toUpperCase()}</a>
 
 
 <style >
@@ -29,9 +29,5 @@
 
   .isFilled {
     background-color: var(--hoverTextColor);
-  }
-
-  .isSmallBtn {
-    width: var(--btn-width-sm);
   }
 </style>
