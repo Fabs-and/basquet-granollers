4fe708b8747ea7ebe21b89a51cd166714a65f30f
fabianlopezdev <fabianlopez@pm.me>
2023-10-26 10:02:33 -0400

    Removed view transitions after testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,8 @@ export default defineConfig({
   // adapter: netlify(),
   //By default Astro will build your site as a static site, but you can also build it as a server-rendered site by setting the output option to server. Change to hybrid to by default being a static site generated
   // output: "hybrid",
+  image: {
+    domains: ["astro.build"],
+  },
   integrations: [svelte()]
 });
