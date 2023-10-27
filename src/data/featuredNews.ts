7459ee639b71c6df78389aa6781c914b635e9e9f
fabianlopezdev <fabianlopez@pm.me>
2023-10-27 18:58:08 -0400

    Deleted mockup data files. Deleted copyright component

--- src/data/featuredNews.ts
+++
@@ -1,8 +0,0 @@
-export const featuredNews = [
-  {
-    title: {rendered: `La història del bàsquet a Granollers`},
-    slug: `la-historia-del-basquet-a-granollers`,
-    image: 'featured-image-mockup.jpg',
-    date: '2021-01-01',
-  }
-]
