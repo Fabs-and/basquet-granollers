f55d89bb0e0e0c22b18764d2f7bb570a7959b2ef
fabianlopezdev <fabianlopez@pm.me>
2023-10-28 18:48:25 -0400

    Testing a way to improve online building and deployment by caching dist and comparing new builds with old builds. Still testing. Change script to ES6

--- compare-and-prepare.js
+++ compare-and-prepare.js
@@ -1,24 +1,32 @@
-const fs = require("fs");
-const path = require("path");
+//Kept in js to avoid unnecessary compilation in github actions
+
+import {
+  readdirSync,
+  existsSync,
+  mkdirSync,
+  statSync,
+  copyFileSync,
+  rmdirSync,
+  renameSync,
+} from "fs";
+import { join, relative, dirname } from "path";
 
 const distDir = "dist";
 const cacheDistDir = "cache-dist";
 const uploadDir = "upload";
 
 // Ensure upload directory exists
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
+if (!existsSync(uploadDir)) {
+  mkdirSync(uploadDir);
 }
 
 // Function to get a list of files in a directory
 function getFiles(dir) {
-  return fs
-    .readdirSync(dir, { withFileTypes: true })
-    .flatMap((dirent) =>
-      dirent.isDirectory()
-        ? getFiles(path.join(dir, dirent.name))
-        : path.join(dir, dirent.name),
-    );
+  return readdirSync(dir, { withFileTypes: true }).flatMap((dirent) =>
+    dirent.isDirectory()
+      ? getFiles(join(dir, dirent.name))
+      : join(dir, dirent.name),
+  );
 }
 
 // Get files from dist and cache-dist directories
@@ -27,18 +35,18 @@ const cacheDistFiles = getFiles(cacheDistDir);
 
 // Compare files and prepare the upload directory
 distFiles.forEach((file) => {
-  const cacheFile = path.join(cacheDistDir, path.relative(distDir, file));
+  const cacheFile = join(cacheDistDir, relative(distDir, file));
   if (
-    !fs.existsSync(cacheFile) ||
-    fs.statSync(file).size !== fs.statSync(cacheFile).size
+    !existsSync(cacheFile) ||
+    statSync(file).size !== statSync(cacheFile).size
   ) {
     // File is new or changed, copy to upload directory
-    const uploadFile = path.join(uploadDir, path.relative(distDir, file));
-    fs.mkdirSync(path.dirname(uploadFile), { recursive: true });
-    fs.copyFileSync(file, uploadFile);
+    const uploadFile = join(uploadDir, relative(distDir, file));
+    mkdirSync(dirname(uploadFile), { recursive: true });
+    copyFileSync(file, uploadFile);
   }
 });
 
 // Update the cache for next run
-fs.rmdirSync(cacheDistDir, { recursive: true });
-fs.renameSync(distDir, cacheDistDir);
+rmdirSync(cacheDistDir, { recursive: true });
+renameSync(distDir, cacheDistDir);
