70bcb861c82a397a4fb960240149ba68690f8484
fabianlopezdev <fabianlopez@pm.me>
2023-10-28 21:06:52 -0400

    Fine tunning the script to be run by github action

--- fetch-server-files.js
+++ fetch-server-files.js
@@ -1,33 +1,33 @@
-// import { Client } from "basic-ftp";
-// import { config } from "dotenv";
-// import { writeFileSync } from "fs";
-// config();
+import { Client } from "basic-ftp";
+import { config } from "dotenv";
+import { writeFileSync } from "fs";
+config();
 
-// async function fetchServerFiles() {
-//   const client = new Client();
-//   try {
-//     await client.access({
-//       host: process.env.FTP_HOST,
-//       user: process.env.FTP_USER,
-//       password: process.env.FTP_PASSWORD,
-//     });
-//     let serverFiles = await client.list();
-//     // Filter out the specified files and directories
-//     serverFiles = serverFiles.filter((file) => {
-//       return !(
-//         file.name === ".htaccess" ||
-//         file.name === ".ftpquota" ||
-//         (file.name === "wordpress" && file.type === 1)
-//       );
-//     });
-//     // Optionally, you could download the files instead of just listing them.
-//     // For simplicity, we're just going to save the metadata to a file.
-//     writeFileSync("server-files.json", JSON.stringify(serverFiles, null, 2));
-//   } catch (error) {
-//     console.error(error);
-//   }
-//   client.close();
-// }
+async function fetchServerFiles() {
+  const client = new Client();
+  try {
+    await client.access({
+      host: process.env.FTP_HOST,
+      user: process.env.FTP_USER,
+      password: process.env.FTP_PASSWORD,
+    });
+    let serverFiles = await client.list();
+    // Filter out the specified files and directories
+    serverFiles = serverFiles.filter((file) => {
+      return !(
+        file.name === ".htaccess" ||
+        file.name === ".ftpquota" ||
+        (file.name === "wordpress" && file.type === 1)
+      );
+    });
+    // Optionally, you could download the files instead of just listing them.
+    // For simplicity, we're just going to save the metadata to a file.
+    writeFileSync("server-files.json", JSON.stringify(serverFiles, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
+  client.close();
+}
 
-// // Call the function to fetch server files
-// fetchServerFiles();
+// Call the function to fetch server files
+fetchServerFiles();
--- ftp-deploy.js
+++ ftp-deploy.js
@@ -1,22 +1,23 @@
-import { Client } from "basic-ftp";
-import { config } from "dotenv";
-config();
+// import { Client } from "basic-ftp";
+// import { config } from "dotenv";
+// config();
 
-async function uploadFiles() {
-  const client = new Client();
-  try {
-    await client.access({
-      host: process.env.FTP_HOST,
-      user: process.env.FTP_USER,
-      password: process.env.FTP_PASSWORD,
-    });
-    // Upload the contents of the upload directory to the root directory on the server
-    await client.uploadFromDir("upload", "/");
-  } catch (error) {
-    console.error(error);
-  }
-  client.close();
-}
+// async function uploadFiles() {
+//   const client = new Client();
+//   try {
+//     await client.access({
+//       host: process.env.FTP_HOST,
+//       user: process.env.FTP_USER,
+//       password: process.env.FTP_PASSWORD,
+//     });
+//     // Upload the contents of the upload directory to the root directory on the server
+//     await client.uploadFromDir("upload", "/");
+//   } catch (error) {
+//     console.error(error);
+//   }
+//   client.close();
+// }
 
-// Call the function to upload files
-uploadFiles();
+// // Call the function to upload files
+// uploadFiles();
+// // 
