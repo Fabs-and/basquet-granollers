f83ab40a495bdfcb86cce245663047ec357be6b9
fabianlopezdev <fabianlopez@pm.me>
2023-10-29 13:37:26 -0400

    Creating a js file to speed up github actions

--- process-files.js
+++ process-files.js
@@ -1,6 +1,6 @@
 import { Client } from "basic-ftp";
 import { config } from "dotenv";
-import { readdirSync, statSync } from "fs";
+import { readdirSync, statSync, readFileSync, writeFileSync } from "fs";
 import { join } from "path";
 
 config(); // Load environment variables from .env file
@@ -11,17 +11,20 @@ async function processFiles() {
   const client = new Client();
 
   try {
-    console.log("Attempting to connect to FTP server...");
+    console.log(
+      `[${new Date().toISOString()}] Attempting to connect to FTP server...`,
+    );
     await client.access({
       host: process.env.FTP_HOST,
       user: process.env.FTP_USER,
       password: process.env.FTP_PASSWORD,
     });
-    console.log("Connected to FTP server.");
+    console.log(`[${new Date().toISOString()}] Connected to FTP server.`);
 
-    console.log("Fetching list of files from server...");
+    console.log(
+      `[${new Date().toISOString()}] Fetching list of files from server...`,
+    );
     let serverFiles = await client.list();
-
     // Filter out specified files and directories from server files list
     serverFiles = serverFiles.filter((file) => {
       return !(
@@ -31,36 +34,89 @@ async function processFiles() {
       );
     });
 
-    console.log("Fetched list of files from server: ", serverFiles);
-    console.log("Fetching list of files from dist directory...");
+    console.log(
+      `[${new Date().toISOString()}] Fetching list of files from dist directory...`,
+    );
     const distFiles = getFiles(distDir);
-    console.log("Fetched list of files from dist directory.");
+    console.log(
+      `[${new Date().toISOString()}] Fetched list of files from dist directory.`,
+    );
 
-    for (const distFile of distFiles) {
+    // Parallel uploading
+    const uploadPromises = distFiles.map(async (distFile) => {
       const serverFile = serverFiles.find(
         (file) => file.name === distFile.name,
       );
 
       if (!serverFile || distFile.size !== serverFile.size) {
         // File is new or has a different size, upload it
-        console.log(`Uploading file: ${distFile.path}...`);
+        console.log(
+          `[${new Date().toISOString()}] Uploading file: ${distFile.path}...`,
+        );
         const uploadPath = join("/", distFile.path); // Adjust path as needed
-        await client.uploadFrom(distFile.path, uploadPath);
-        console.log(`Uploaded file: ${distFile.path}.`);
+        try {
+          await client.uploadFrom(distFile.path, uploadPath);
+          console.log(
+            `[${new Date().toISOString()}] Uploaded file: ${distFile.path}.`,
+          );
+        } catch (error) {
+          console.error(
+            `[${new Date().toISOString()}] Failed to upload file: ${
+              distFile.path
+            }.`,
+            error,
+          );
+        }
       }
-    }
+    });
+    await Promise.all(uploadPromises);
+
+    // Identify extra files on server
+    const serverFileNames = serverFiles.map((file) => file.name);
+    const distFileNames = distFiles.map((file) => file.name);
+    const filesToDelete = serverFileNames.filter(
+      (file, index) =>
+        !distFileNames.includes(file) &&
+        !(
+          (file === ".htaccess" && serverFiles[index].type === 0) ||
+          (file === ".ftpquota" && serverFiles[index].type === 0) ||
+          (file === "wordpress" && serverFiles[index].type === 1)
+        ),
+    );
+
+    // Delete extra files from server
+    // Parallel deletion
+    const deletePromises = filesToDelete.map(async (file) => {
+      console.log(`[${new Date().toISOString()}] Deleting file: ${file}...`);
+      try {
+        // Check if it's a file or directory
+        const isDirectory = serverFiles.find(
+          (f) => f.name === file && f.type === 1,
+        );
+        if (isDirectory) {
+          // If it's a directory, remove the directory
+          await client.removeDir(file);
+        } else {
+          // If it's a file, remove the file
+          await client.remove(file);
+        }
+        console.log(`[${new Date().toISOString()}] Deleted file: ${file}.`);
+      } catch (error) {
+        console.error(
+          `[${new Date().toISOString()}] Failed to delete file: ${file}.`,
+          error,
+        );
+      }
+    });
+    await Promise.all(deletePromises);
   } catch (error) {
-    console.error("Error encountered:", error);
+    console.error(`[${new Date().toISOString()}] Error encountered:`, error);
     process.exit(1);
   } finally {
-    console.log("Closing FTP client...");
-    client.close();
-    console.log("FTP client closed.");
+    console.log(`[${new Date().toISOString()}] Closing FTP client...`);
+    await client.close();
+    console.log(`[${new Date().toISOString()}] FTP client closed.`);
   }
-
-  console.log("Closing FTP client...");
-  client.close();
-  console.log("FTP client closed.");
 }
 
 // Helper function to recursively list all files in specified directory
