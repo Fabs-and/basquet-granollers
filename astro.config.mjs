4179bc30475ac9b54027a87841fb3db2c05926ab
fabianlopezdev <fabianlopez@pm.me>
2023-11-21 16:22:21 -0500

    Reverted back to older astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
-// import hnodejs from "@astrojs/node";
+
 
 // import netlify from "@astrojs/netlify/functions";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  // adapter: netlify(),
+
   //By default Astro will build your site as a static site, but you can also build it as a server-rendered site by setting the output option to server. Change to hybrid to by default being a static site generated
   // output: "hybrid",
   integrations: [svelte()]
